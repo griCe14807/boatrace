4571fdaab54f97d61a44d94b8276ce169bdbbbb1
griCe14807 <griCe14807@outlook.jp>
2019-10-20 21:55:37 +0900

    add

--- src/automated_voting/voting_algolithms/LR_voter.py
+++ src/automated_voting/voting_algolithms/LR_voter.py
@@ -160,6 +160,8 @@ def main(rno, jcd, hd):
     for i, clf in enumerate(clf_list):
         predict_proba_all.append(clf.predict_proba(x)[0][1])
 
+    print(predict_proba_all)
+
     # 投票するリストを作成 (1頭でx_2を超えたやつとx_3を超えたやつの組み合わせbox
     voting_number_list_1 = voting_algolithm_1(predict_proba_all, 0.6, 0.8, 0.7)
     voting_number_list_2 = voting_algolithm_2(predict_proba_all, 0.5, 0.8, 0.6)
